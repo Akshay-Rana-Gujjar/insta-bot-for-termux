31e353088ab660034db1f7aa3d0f39fe0caccbb2
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2020-07-25 19:15:21 +0530

    update SESSION_PATH variable

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var profileIndexFromCLI = process.argv[2];
 var config = require("./config")[profileIndexFromCLI];
 var { exec } = require("child_process");
 var inquirer = require("inquirer");
+var SESSION_PATH = __dirname+"/sessions";
 
-const igc = new IgApiClient();
 
 function sendNotification(text) {
     exec(`termux-notification -c '${text}'`);
@@ -24,6 +24,72 @@ if (!config) {
 const IG_USERNAME = config.USERNAME, IG_PASSWORD = config.PASSWORD;
 const IMAGE_PATH_DIR = config.IMAGEPATHDIR;
 
+process.env["config.seed"] = `${IG_USERNAME}-seed`;
+
+const igc = new IgApiClient();
+
+const saveSession = async (data, username) => {
+    if (!fs.existsSync(SESSION_PATH)) {
+        fs.mkdirSync(SESSION_PATH);
+    }
+
+    fs.writeFileSync(`${SESSION_PATH}/${username}.json`, JSON.stringify(data), { encoding: "utf8" });
+};
+
+const loadSession = async (username) => {
+    log("reading file...");
+    const sessionFile = fs.readFileSync(`${SESSION_PATH}/${username}.json`, "utf8");
+    await igc.simulate.preLoginFlow();
+    log("loading session...");
+    await igc.state.deserialize(sessionFile);
+    process.nextTick(async () => await igc.simulate.postLoginFlow());
+    igc.request.end$.subscribe(async () => {
+        const serialized = await igc.state.serialize();
+        delete serialized.constants;
+        log("saving session state...");
+        await saveSession(serialized, username);
+    });
+    const pk = igc.state.cookieUserId;
+    log("session loaded...");
+    return pk;
+};
+
+const newLogin = async (data) => {
+    const userCred = data;
+    await igc.simulate.preLoginFlow();
+    log("Loging in...");
+    try {
+
+        const auth = await igc.account.login(userCred.username, userCred.password);
+        process.nextTick(async () => await igc.simulate.postLoginFlow());
+        igc.request.end$.subscribe(async () => {
+            const serialized = await igc.state.serialize();
+            delete serialized.constants;
+            log("saving login state...");
+            await saveSession(serialized, userCred.username);
+        });
+        log("user logged in...");
+        return auth.pk;
+    } catch (error) {
+        sendNotification("Error while login");
+        throw new Error(error);
+    }
+};
+
+
+var loginFlow = async (data) => {
+    log("Checking file....");
+    const igSession = fs.existsSync(`${SESSION_PATH}/${data.username}.json`);
+    if (igSession) {
+        log("File found! \nLoadind Session... ");
+        const auth = await loadSession(data.username); // load session by username
+        return auth;
+    } else {
+        log("File Not Found, Login...");
+        const auth = await newLogin(data);
+        return auth;
+    }
+};
 
 var login = async function login1() {
     log("generating device...");
@@ -31,29 +97,33 @@ var login = async function login1() {
     await igc.simulate.preLoginFlow();
     log("Device generated, loging in.....");
     try {
-        var login = await igc.account.login(IG_USERNAME, IG_PASSWORD);
-    } catch (error) {
-        log(error);
-        console.log(igc.state.checkpoint); // Checkpoint info here
-        await igc.challenge.auto(true); // Requesting sms-code or click "It was me" button
-        console.log(igc.state.checkpoint); // Challenge info here
-        const { code } = await inquirer.prompt([
-            {
-                type: 'input',
-                name: 'code',
-                message: 'Enter code',
-            },
-        ]);
-        var status = await igc.challenge.sendSecurityCode(code);
 
-        if (status.status == "ok") {
-            return;
-        } else {
-            return sendNotification("Error While login");
-        }
+        await loginFlow({ username: IG_USERNAME, password: IG_PASSWORD });
+
+    } catch (error) {
+
+        log(error);
+        // console.log(igc.state.checkpoint); // Checkpoint info here
+        // await igc.challenge.auto(true); // Requesting sms-code or click "It was me" button
+        // console.log(igc.state.checkpoint); // Challenge info here
+        // const { code } = await inquirer.prompt([
+        //     {
+        //         type: 'input',
+        //         name: 'code',
+        //         message: 'Enter code',
+        //     },
+        // ]);
+        // var status = await igc.challenge.sendSecurityCode(code);
+
+        // if (status.status == "ok") {
+        //     return;
+        // } else {
+        sendNotification("Error While login");
+        throw new Error(error);
+        // }
     }
 
-}
+};
 
 function publishAndCropImage(fileName, cb) {
 
--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@
 - [ ] Hashtag shuffling
 
 ## 24 July 2020 
- - [ ] Adding state management for not logging again and again
+ - [x] Adding state management for not logging again and again
  - [ ] Add NeDB as lightweight Database
